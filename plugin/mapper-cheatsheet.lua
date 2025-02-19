f9634a61c026f09d8c75b4374d1238c8b0fc09e9
Jack Mortimer <jackmortimer33@gmail.com>
2025-02-19 00:50:56 +0000

    Update mapper-cheatsheet.lua

--- plugin/mapper-cheatsheet.lua
+++ plugin/mapper-cheatsheet.lua
@@ -1,4 +1,4 @@
 local cheatsheet = require('mapper-cheatsheet')
-vim.api.nvim_create_user_command('DeciCheatsheet', function()
+vim.api.nvim_create_user_command('MapperCheatsheet', function()
     cheatsheet.show_cheatsheet()
 end, {})
